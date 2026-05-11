e1d8e4860fd2fca194e1ea6b9d7822087d2f31c2
hwilson23 <111527077+hwilson23@users.noreply.github.com>
2026-05-11 12:24:23 -0500

    fixed comment error

--- collagen_3D_multimetric-main/src/collagen_dataframe/ctfire_statssummary.py
+++ collagen_3D_multimetric-main/src/collagen_dataframe/ctfire_statssummary.py
@@ -10,14 +10,14 @@ df = pd.read_csv('G:\\FluorescentCollagen\\20260302_ows2_col\\results_test_maske
    # .reset_index()
     #.rename(columns={'mean': 'mean_of_slices'})
     #.pivot(index='stack',columns='hist_type', values = 'mean_of_slices')
-)
+#)
 
 # Random slice per stack + hist_type
 #random_slice = (
  #   df.groupby(['stack', 'hist_type'])
   #  .apply(lambda x: x.sample(1), include_groups=False)
    # .reset_index(drop=True)
-)
+#)
 
 #print("Mean across all slices:")
 #print(mean_all_slices)
